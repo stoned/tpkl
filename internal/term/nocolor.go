1b82c651b57ffcfaf759bcb3697eadc36276cf1a
Stoned Elipot <stoned.elipot@proton.me>
2026-07-11 23:22:42 +0200

    doc: update references

--- internal/term/nocolor.go
+++ internal/term/nocolor.go
@@ -13,8 +13,10 @@ import (
 // It uses github.com/charmbracelet/colorprofile.
 //
 // https://github.com/charmbracelet/colorprofile
-// https://bixense.com/clicolors/
+// https://force-color.org/
 // https://no-color.org/
+// https://web.archive.org/web/20260616201813/https://no-color.org/
+// https://bixense.com/clicolors/
 func NoColor(output io.Writer) bool {
 	p := colorprofile.Detect(output, os.Environ())
 	switch p { //nolint:exhaustive
